2d83c12ab8a9686a2878d308959eb33c5230aea5
Rayhaan Bhikha <Rayhaanbhikha3@hotmail.com>
2025-03-11 19:52:58 +0000

    finally working

--- main.go
+++ main.go
@@ -4,32 +4,79 @@ import (
 	"flag"
 	"fmt"
 	"log"
-
+	"os"
+	"path"
 	"strings"
 )
 
+// TODOs:
+// - support variadic parameters
+// - support searching through nested directories
+// - quiet flag not used yet.
+
 func main() {
-	structName := flag.String("type", "", "Name of the struct to generate builder for")
-	// TODO: is this needed?
-	inputFile := flag.String("file", "", "Go source file containing the struct")
-	flag.Parse()
+	config := parseConfigFromArgs(os.Args)
 
-	if *structName == "" || *inputFile == "" {
-		log.Fatal("Usage: structBuilder -type=StructName -file=source.go")
-	}
-
-	structInfo, err := parseGoFile(*inputFile, *structName)
+	structInfo, err := parseDir(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("StructInfo: ", structInfo)
+	fmt.Printf("StructInfo: %v\n", structInfo)
 
-	outputFile := fmt.Sprintf("%s_builder.go", strings.ToLower(*structName))
-	err = create(outputFile, structInfo)
+	// create the output directory if it doesn't exist
+	err = os.MkdirAll(config.outputDir, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Generated %s\n", outputFile)
+	// Create the file
+	file, err := os.Create(config.outputFile())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	err = executeTemplate(structInfo, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Generated %s\n", config.outputFile())
+}
+
+type Config struct {
+	structName string
+	dir        string
+	outputDir  string
+	outputPkg  string
+	quiet      bool
+}
+
+func (c *Config) outputFile() string {
+	return fmt.Sprintf("%s/%s_builder.go", c.outputDir, strings.ToLower(c.structName))
+}
+
+func parseConfigFromArgs(args []string) *Config {
+	config := &Config{}
+
+	flagSet := flag.NewFlagSet(args[0], flag.ExitOnError)
+
+	flagSet.StringVar(&config.structName, "structname", "", "Name of the struct to generate builder for")
+	flagSet.StringVar(&config.outputDir, "outputdir", "", "Directory to write the generated builder to")
+	flagSet.StringVar(&config.outputPkg, "outpkg", "builders", "Name of the generated package")
+	flagSet.StringVar(&config.dir, "dir", ".", "Directory to search for the struct definition")
+	flagSet.BoolVar(&config.quiet, "quiet", false, "suppress output to stdout")
+
+	flagSet.Parse(args[1:])
+
+	if config.structName == "" {
+		log.Fatal("Usage: structBuilder -structName=StructName")
+	}
+
+	if config.outputDir == "" {
+		config.outputDir = path.Join(config.dir, "builders")
+	}
+
+	return config
 }
--- parser.go
+++ parser.go
@@ -3,38 +3,76 @@ package main
 import (
 	"fmt"
 	"go/ast"
-	"go/parser"
-	"go/token"
+	"golang.org/x/tools/go/packages"
 )
 
-func parseGoFile(filename, structName string) (*StructInfo, error) {
-	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, filename, nil, parser.AllErrors)
+func parseDir(config *Config) (*StructInfo, error) {
+	cfg := &packages.Config{
+		Mode:  packages.NeedSyntax | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedName,
+		Tests: false,
+		Dir:   config.dir,
+	}
+
+	// Load all packages in the module or a specific subset
+	// You can specify patterns like "./..." for the current module recursively or specific paths.
+	pkgs, err := packages.Load(cfg, "./...")
 	if err != nil {
 		return nil, err
 	}
 
+	for _, pkg := range pkgs {
+
+		// Iterate over each file in the package
+		for _, syn := range pkg.Syntax {
+			structInfo := parseFileDeclarations(syn, config.structName)
+			if structInfo != nil {
+
+				structInfo.OutputPackageName = config.outputPkg
+				structInfo.ImportPath = pkg.PkgPath
+				structInfo.OutputTypeName = fmt.Sprintf("%s.%s", pkg.Name, config.structName)
+
+				return structInfo, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("struct %s not found in directory %s", config.structName, config.dir)
+}
+
+func parseFileDeclarations(file *ast.File, structName string) *StructInfo {
 	for _, decl := range file.Decls {
 		if genDecl, ok := decl.(*ast.GenDecl); ok {
 			for _, spec := range genDecl.Specs {
-				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
-					if structType, ok := typeSpec.Type.(*ast.StructType); ok && typeSpec.Name.Name == structName {
-						var fields []StructField
-						for _, field := range structType.Fields.List {
-							for _, name := range field.Names {
-								// do we care if the field is Uppercased or not?
-								fields = append(fields, StructField{Name: name.Name, Type: fmt.Sprint(field.Type)})
-							}
-						}
-						return &StructInfo{
-							PackageName: file.Name.Name,
-							TypeName:    structName,
-							Fields:      fields,
-						}, nil
+				typeSpec, ok := spec.(*ast.TypeSpec)
+				if !ok {
+					continue
+				}
+
+				structType, ok := typeSpec.Type.(*ast.StructType)
+				if !ok {
+					continue
+				}
+
+				if typeSpec.Name.Name != structName {
+					continue
+				}
+
+				var fields []StructField
+
+				for _, field := range structType.Fields.List {
+					for _, name := range field.Names {
+						// do we care if the field is Uppercased or not?
+						fields = append(fields, StructField{Name: name.Name, Type: fmt.Sprint(field.Type)})
 					}
+				}
+
+				return &StructInfo{
+					BuilderTypeName: structName,
+					Fields:          fields,
 				}
 			}
 		}
 	}
-	return nil, fmt.Errorf("struct %s not found", structName)
+
+	return nil
 }
